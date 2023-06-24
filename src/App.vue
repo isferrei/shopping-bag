74b7b28b8066f82a486e487607ec58c5cc0bd25d
Isabela Carvalho <isabelad-carvalho@hotmail.com>
2023-06-23 23:15:20 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <nav>
+  <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <router-link to="/shoppingBag">Cart ({{this.productsInBag.length}})</router-link>
+  </div>
+  <router-view />
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  created() {
+    this.$store.dispatch("loadProducts");
+    this.$store.dispatch("loadBag");
+  },
+  computed: mapState(['productsInBag'])
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,15 +27,24 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+#nav {
+  padding: 16px 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  background-color: rgb(37, 37, 37);
+  color: white;
+
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    color: white;
+    text-decoration: none;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #007bff;
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,52 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    products:[],
+    productsInBag:[]
   },
   getters: {
   },
   mutations: {
+    loadProducts(state, products){
+      state.products = products
+    },
+
+    loadBag(state, products){
+      state.productsInBag = products
+    },
+
+    addToBag(state, product){
+      state.productsInBag.push(product)
+      localStorage.setItem("productsInBag", JSON.stringify(state.productsInBag))
+    },
+
+    removeFromBag(state, productId){
+      var updatedBag = state.productsInBag.filter((item) => item.id != productId)
+      state.productsInBag = updatedBag;
+    }
   },
   actions: {
+    loadProducts({commit}){
+      axios.get('https://fakestoreapi.com/products').then(response => commit('loadProducts',response.data))
+    },
+
+    loadBag({commit}){
+      if(localStorage.getItem("productsInBag")){
+        commit('loadBag', JSON.parse(localStorage.getItem("productsInBag")) )
+      }
+    },
+
+    addToBag({commit}, product){
+      commit('addToBag', product)
+    },
+
+    removeFromBag({commit}, productId){
+      if(confirm('Are you sure you want to remove this item?')){
+        commit('removeFromBag', productId) 
+      }
+    }
   },
   modules: {
   }
